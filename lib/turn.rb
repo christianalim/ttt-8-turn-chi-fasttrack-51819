3f826f1ae6a12581e76afa8bc63d4a350936a7f7
Christian Lim <christiananthonylim1@gmail.com>
2019-05-19 02:27:56 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,3 +21,11 @@ def valid_move?(board, index)
     false
   end
 end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] ==  "O"
+    true
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  end
+end
